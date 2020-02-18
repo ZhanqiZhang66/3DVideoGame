5b4043ba96997f48bfc4afae58641538a53a0beb
lanchen <lanchen@wustl.edu>
2020-02-17 19:55:15 -0600

    Pipe Rotation Done
    Add scripts for pipe rotation(DONE) and Ball moving forward(NOT YET)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Outlets
+    Rigidbody rb;
+
+    // Configuration
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Go forward
+        transform.position = Vector3.forward * speed;
+
+    }
+}
---
+++ Assets/Scripts/RotateView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Rotate our platform 
+            transform.Rotate(0f, 0f, 120f);
+        }
+        
+    }
+}
