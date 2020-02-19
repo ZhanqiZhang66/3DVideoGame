86ff4dc8bade1fb60af3f18852633d54bd6085d7
ZhanqiZhang66 <31415280+ZhanqiZhang66@users.noreply.github.com>
2020-02-19 14:40:51 -0600

    beat detector and background music done

---
+++ Assets/Scripts/DontDestroyAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
