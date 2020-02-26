2415f33d8f85f5c9c179b42e826f446823708d21
Jinlin <zhangjinlin0226@gmail.com>
2020-02-26 17:44:31 -0600

    menu

---
+++ Assets/Scripts/but.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class but : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	public void play(){
+		SceneManager.LoadScene ("SampleScene");
+	}
+	
+	// Update is called once per frame
+	public void tutorial(){
+		//SceneManager.LoadScene ("start menu 1");
+	}
+
+	public void back(){
+		//SceneManager.LoadScene ("start menu");
+	}
+
+	public void next() {
+		//SceneManager.LoadScene ("Tutorial_Star");
+	}
+
+	public void exit(){
+		UnityEditor.EditorApplication.isPlaying = false;
+		Application.Quit();
+	}
+
+
+
+
+}
