7eb993c33f9a643c5578bba40a2e8af1195a4cdf
Jinlin <zhangjinlin0226@gmail.com>
2020-04-11 15:14:36 -0500

    no message

---
+++ Assets/Scripts/Canvas.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Canvas : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject pause_panel;
+	public GameObject game_panel;
+	public AudioSource bgm;
+	
+	public void play(){
+		SceneManager.LoadScene ("SampleScene");
+	}
+	
+	// Update is called once per frame
+	public void tutorial(){
+		//SceneManager.LoadScene ("start menu 1");
+	}
+
+	public void back(){
+		//SceneManager.LoadScene ("start menu");
+	}
+
+	public void next() {
+		//SceneManager.LoadScene ("Tutorial_Star");
+	}
+
+	public void exit(){
+		UnityEditor.EditorApplication.isPlaying = false;
+		Application.Quit();
+	}
+	
+	public void Restart(){
+		bgm.Stop();
+		SceneManager.LoadScene ("Try");
+		pause_panel.SetActive (false);
+		game_panel.SetActive (true);
+	}
+	
+	public void MainMenu(){
+		SceneManager.LoadScene ("start menu");
+	}
+	
+	public void Pause(){
+		
+		Time.timeScale = 0;
+		bgm.Pause();
+		pause_panel.SetActive (true);
+		game_panel.SetActive (false);
+	}
+
+	public void Unpause(){
+		Time.timeScale = 1;
+		bgm.Play();
+		pause_panel.SetActive (false);
+		game_panel.SetActive (true);
+	}
+}
---
+++ Assets/Scripts/Combo.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Combo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI comboC;
+    private static int score;  //  A new Static variable to hold our score.
+    private static int fontSize;
+    void Start(){
+    comboC = GetComponent<TextMeshProUGUI>();
+    score = 0;
+    fontSize = 40;
+    }
+    void Update()
+    {
+        score = GameObject.Find("GameController").GetComponent<GameController>().comboCount;
+	comboC.fontSize = 40 + (score * 5);
+        if(score == 0) {
+	comboC.text = "";
+	} else {
+	comboC.text = "X" + score;
+	}
+    }
+
+    
+}
